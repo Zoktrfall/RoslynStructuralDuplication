43e5a0517d9de73f65dcccd4e726712ca8a62864
ZoktrFall <afrikyanarman24@gmail.com>
2025-10-26 21:54:34 +0400

    Start RoslynStructuralDuplication

---
+++ RoslynStructuralDuplication/Program.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.CodeAnalysis.CSharp;
+
+namespace RoslynStructuralDuplication;
+
+class Program
+{
+    private sealed class StructuralDuplicateFeature : CSharpSyntaxRewriter
+    {
+        public string ProcessSingleParameterMethods(string content)
+        {
+            var syntaxTree = CSharpSyntaxTree.ParseText(content);
+            var rootNode = syntaxTree.GetRoot();
+            
+            Console.WriteLine(rootNode.Kind());  
+            
+            foreach (var node in rootNode.DescendantNodes())
+                Console.WriteLine(node.Kind());
+
+            return "";
+        }
+    }
+    
+    static int Main(string[] args)
+    {
+        if (args.Length != 1)
+        {
+            Console.WriteLine("Usage: RoslynStructuralDuplication.exe <input.cs> ");
+            return 1;
+        }
+        
+        var inputFile = args[0];
+        if (!File.Exists(inputFile))
+        {
+            Console.WriteLine($"File {inputFile} does not exist.");
+            return 2;
+        }
+
+        try
+        {
+            var feature = new StructuralDuplicateFeature();
+            
+            var inputFileContent = File.ReadAllText(inputFile);
+            var outputFileContent = feature
+                .ProcessSingleParameterMethods(inputFileContent);
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+        }
+        
+        return 0;
+    }
+}
